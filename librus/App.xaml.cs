6a1371dca6e95fa2eb675b55329b85970d11bcb9
LukaszJablonski <Lukasz.jablonski44@gmail.com>
2024-01-19 11:32:45 +0100

    Added_TabbedPage_Style

--- librus/App.xaml.cs
+++ librus/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,6 +7,14 @@ namespace librus
 {
     public partial class App : Application
     {
+        static Database database;
+
+        public static Database Database { get {
+                if (database == null)
+                {
+                    database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "librus.db3"));
+                }
+                return database; } }
         public App()
         {
             InitializeComponent();
